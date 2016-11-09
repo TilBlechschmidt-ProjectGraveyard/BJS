c7b4d51bb3da4114fcaaddca506f5667f1e469db
Til Blechschmidt <til@blechschmidt.de>
2016-11-09 18:36:48 +0000

    Added lodash package and moved pwdgen code

--- meteor/imports/startup/server/tests.js
+++ meteor/imports/startup/server/tests.js
@@ -1,5 +1,6 @@
 import {CompetitionTypes} from "../../api/logic/competition_type";
 import {Athlete} from "../../api/logic/athlete";
+import {genRandomPassword} from "./pwdgen";
 
 export function tests() {
     console.log(CompetitionTypes[0].object.getSports());
@@ -9,4 +10,8 @@ export function tests() {
     console.log(p.checkPerson());
     console.log(p.getFullName());
     console.log(p.getShortName());
+
+    for (var n in _.range(10)) {
+        console.log(genRandomPassword(100))
+    }
 }
