eb565f82ed71b969a97d17b56fb76bc3610c6910
NoahPeeters <noah.peeters@icloud.com>
2016-11-09 20:59:10 +0100

    Moved data to data directory

--- meteor/imports/api/logic/competition_types/athletics.js
+++ meteor/imports/api/logic/competition_types/athletics.js
@@ -2,22 +2,7 @@ export {Athletics};
 
 let Athletics = {
     getSports: function () {
-        return [
-            {
-                name: 'Sprint 50m',
-                id: 'sp_sprint_50',
-                category: 0,
-                select_electronic: true,
-                description: ''
-            },
-            {
-                id: 'sp_long_jump',
-                name: 'Weitsprung',
-                category: 1,
-                select_electronic: false,
-                description: 'Jede Teilnehmerin bzw. jeder Teilnehmer hat drei Versuche. Der Absprung von einer Absprungfläche ist zulässig. Diese erstreckt sich von den Kanten des Absprungbalkens 30 cm nach beiden Seiten, umfasst also insgesamt 80 cm. Die Sprungweite wird vom hintersten Eindruck des Niedersprungs bis zur Höhe des vordersten Eindrucks des Absprungs gemessen, wobei die Messung immer im rechten Winkel zum Balken vorgenommen wird. Gültigkeit der Sprünge siehe Zeichnung.'
-            }
-        ];
+        return require('./../../data/athletics_sports.json');
     },
 
 
@@ -42,9 +27,6 @@ let Athletics = {
     },
 
     getInformation: function () {
-        return {
-            'Allgemein': '',
-            'Bewertung': ''
-        };
+        return require('./../../data/athletics_information.json');
     },
 };
