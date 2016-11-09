c7822394a3baad65969b2e24e0dcd78bd9f0fbca
Til Blechschmidt <til@blechschmidt.de>
2016-11-09 19:12:30 +0000

    Removed old comment block

--- meteor/server/main.js
+++ meteor/server/main.js
@@ -1,41 +1,6 @@
-import { Meteor } from "meteor/meteor";
+import {Meteor} from "meteor/meteor";
 
 Meteor.startup(function () {
     import {onStartup} from '../imports/startup/server/index.js';
     onStartup();
 });
-
-/*
-
-function tests() {
-    console.log(CompetitionTypes[0].object.getSports());
-
-    var p = new Athlete('Hans', 'Peter', 16, true, 'Q#z', 'A0');
-
-    console.log(p.checkPerson());
-    console.log(p.getFullName());
-    console.log(p.getShortName());
-
-    for (var n in _.range(10)) {
-        console.log(getRandomPassword(100))
-    }
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomPassword(length) {
-    var pass = "";
-    for (var i in _.range(length)) {
-        var code = getRandomInt(0, 34);
-        if (code < 9) {
-            pass += String.fromCharCode(49 + code);
-        } else {
-            pass += String.fromCharCode(56 + code);
-        }
-    }
-    return pass;
-}
-
-*/
