a632f4a4a52192be97a57bfed543992971a73f15
Til Blechschmidt <til@blechschmidt.de>
2016-11-15 20:59:08 +0000

    Added creation of mock data for the Generic table + generic cleanup

--- meteor/imports/api/database/collections/generic.js
+++ meteor/imports/api/database/collections/generic.js
@@ -1,3 +1,10 @@
 import {Collection} from './collection';
 
 export let Generic = new Collection('Generic');
+
+Generic.createMockData = function () {
+    this.handle.insert({
+        dbVersion: Meteor.config.dbVersion,
+        cleanDB: true
+    });
+};
--- meteor/imports/startup/server/index.js
+++ meteor/imports/startup/server/index.js
@@ -1,30 +1,35 @@
 import {COLLECTIONS} from '../../api/database/collections/collection';
 import {resetDatabase} from 'meteor/xolvio:cleaner';
 
-function clearDatabase(dbVersion) {
+function clearDatabase() {
     console.log('-----------------------------------------------------');
     console.log('---------------------- WARNING ----------------------');
     console.log('-----------------------------------------------------');
     console.log('                  Database not clean                 ');
     console.log('Clearing database and adding mock data entries . . .');
+
+    // Delete everything
     resetDatabase();
+
+    // Fill it with (fake) data again
     for (let collection in COLLECTIONS)
         if (COLLECTIONS.hasOwnProperty(collection)) COLLECTIONS[collection].createMockData();
+
     console.log('-----------------------------------------------------');
     console.log('------------------------ DONE -----------------------');
     console.log('-----------------------------------------------------');
 }
 
 export function onStartup() {
-    const dbVersion = require('../../../config.json').dbVersion;
+    // Load the config.json into the (semi-global) Meteor.config object
+    Meteor.config = require('../../../config.json');
+
+    // Check if the database is clean and whether or not its structure is outdated (and possibly recreate it if that's the case)
     let genericEntries = COLLECTIONS.Generic.handle.find({}).fetch();
     //noinspection JSUnresolvedVariable
     if (!(
             genericEntries.length > 0 &&
             (genericEntries[0].hasOwnProperty('cleanDB') && genericEntries[0].cleanDB === true) &&
-            (genericEntries[0].hasOwnProperty('dbVersion') && genericEntries[0].dbVersion === dbVersion)
-        )
-    ) {
-        clearDatabase(dbVersion);
-    }
+            (genericEntries[0].hasOwnProperty('dbVersion') && genericEntries[0].dbVersion === Meteor.config.dbVersion)
+        )) clearDatabase();
 }
