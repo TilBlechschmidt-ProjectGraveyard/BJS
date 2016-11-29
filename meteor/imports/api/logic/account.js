6cf7542bf5b841e56d74278d56792f88f598d4ba
NoahPeeters <noah.peeters@icloud.com>
2016-11-29 17:32:56 +0100

    Removed log object from Account

--- meteor/imports/api/logic/account.js
+++ meteor/imports/api/logic/account.js
@@ -10,7 +10,7 @@
  * @param {*} ac
  * @constructor
  */
-export function Account(log, score_write_permissions, group_permissions, ac) {
+export function Account(score_write_permissions, group_permissions, ac) {
     this.score_write_permissions = score_write_permissions;
     this.group_permissions = group_permissions;
     this.ac = ac;
