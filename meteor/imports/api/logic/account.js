7bc57f8339d3a24debc387155808434531b791cb
NoahPeeters <noah.peeters@icloud.com>
2016-11-29 17:32:22 +0100

    Added account type documentation

--- meteor/imports/api/logic/account.js
+++ meteor/imports/api/logic/account.js
@@ -2,6 +2,14 @@
  * Created by noah on 11/29/16.
  */
 
+/**
+ *
+ * @param log
+ * @param {string[]} score_write_permissions
+ * @param {string[]} group_permissions
+ * @param {*} ac
+ * @constructor
+ */
 export function Account(log, score_write_permissions, group_permissions, ac) {
     this.score_write_permissions = score_write_permissions;
     this.group_permissions = group_permissions;
