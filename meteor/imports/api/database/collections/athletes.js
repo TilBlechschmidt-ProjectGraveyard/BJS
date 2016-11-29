b55cbef1d5165c0919c1e9e7410501719acbbfb6
Til Blechschmidt <til@blechschmidt.de>
2016-11-29 17:00:12 +0000

    Added mock data for athlete

--- meteor/imports/api/database/collections/athletes.js
+++ meteor/imports/api/database/collections/athletes.js
@@ -1,9 +1,14 @@
+import {COMPETITION_TYPES} from "../../logic/competition_type";
+import {Athlete} from '../../logic/athlete';
+import {Log} from "../../log";
+import {generateAC} from "../../crypto/crypto";
 import {Collection} from './collection';
 
 export let Athletes = new Collection('Athletes', true);
 
 Athletes.createMockData = function () {
-    this.handle.insert({
-        name: "Someone"
-    });
+    const log = new Log();
+    const ct = COMPETITION_TYPES[0].object;
+    const ac = generateAC("potato", "pepper");
+    this.handle.insert(new Athlete(log, 'Hans', 'Müller', 2000, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(ac));
 };
