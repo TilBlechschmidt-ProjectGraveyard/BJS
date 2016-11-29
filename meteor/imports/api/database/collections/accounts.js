d12b342e7cb404b3937b4d32895afe8875280c6c
Til Blechschmidt <til@blechschmidt.de>
2016-11-29 17:39:18 +0000

    Added mock data for accounts

--- meteor/imports/api/database/collections/accounts.js
+++ meteor/imports/api/database/collections/accounts.js
@@ -1,3 +1,12 @@
+import {Account} from '../../logic/account';
+import {generateAC} from "../../crypto/crypto";
 import {Collection} from './collection';
 
 export let Accounts = new Collection('Accounts', true);
+
+Accounts.createMockData = function () {
+    const ac = generateAC("potato", "pepper");
+    this.handle.insert(new Account(["st_sprint_50_el"], [], ac));
+    this.handle.insert(new Account([], ["Q#a"], ac));
+    this.handle.insert(new Account(["st_sprint_50_el", "st_sprint_50"], ["Q#z"], ac));
+};
