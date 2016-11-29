5f87f74bb66a2e977b2feb02e55865e730c709e4
NoahPeeters <noah.peeters@icloud.com>
2016-11-29 16:30:13 +0100

    Added test for athlete encryption and decryption

--- meteor/server/test/athlete.test.js
+++ meteor/server/test/athlete.test.js
@@ -7,6 +7,8 @@ chai.should();
 
 const ct = COMPETITION_TYPES[0].object;
 
+const groupAC = generateAC('1234567ljhfaljawf8');
+
 describe('athlete', function () {
     it('changes the age of an athlete', function () {
         const log = new Log();
@@ -15,5 +17,20 @@ describe('athlete', function () {
         p.age = p.age + 1;
 
         p.ageGroup.should.be.equal(1999);
+    });
+
+    it('encrypts and decrypts an athlete', function () {
+        const log = new Log();
+        const p = new Athlete(log, 'Hans', 'Müller', 2000, true, 'Q#z', '0', ct.maxAge, ct);
+        const encrypted_p = p.encryptForDatabase(groupAC);
+        const decrypted_p = Athlete.prototype.decryptFromDatabase(log, encrypted_p, [groupAC]);
+
+        decrypted_p.firstName.should.be.equal(p.firstName);
+        decrypted_p.lastName.should.be.equal(p.lastName);
+        decrypted_p.ageGroup.should.be.equal(p.ageGroup);
+        decrypted_p.isMale.should.be.equal(p.isMale);
+        decrypted_p.group.should.be.equal(p.group);
+        decrypted_p.handicap.should.be.equal(p.handicap);
+        decrypted_p.maxAge.should.be.equal(p.maxAge);
     });
 });
