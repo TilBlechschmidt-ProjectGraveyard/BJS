f9f02117409bfc237199f2d689e6cf1bc23aefbc
Til Blechschmidt <til@blechschmidt.de>
2016-12-26 23:29:50 +0000

    Added optional publicationFunction override

--- meteor/imports/api/database/collections/collection.js
+++ meteor/imports/api/database/collections/collection.js
@@ -1,9 +1,7 @@
-export function Collection(name, grounded, nonCompetitionDB) {
+export function Collection(name, grounded, nonCompetitionDB, publicationFunction) {
     const col = this;
 
     col.name = name;
-
-    console.log("Init database " + name);
 
     col.handle = new Mongo.Collection(col.name, nonCompetitionDB ? {} : {_driver: Meteor.dbHandle});
     console.log(nonCompetitionDB ? {} : {_driver: Meteor.dbHandle});
@@ -13,13 +11,19 @@ export function Collection(name, grounded, nonCompetitionDB) {
     col.createMockData = function () {
     };
 
+    col.publish = publicationFunction ? publicationFunction : function () {
+            if (Meteor.isServer) {
+                Meteor.publish(col.name, function () {
+                    return col.handle.find({});
+                });
+            }
+        };
+
     if (Meteor.isClient && col.grounded) Ground.Collection(col.handle);
 
-    if (Meteor.isServer) {
-        Meteor.publish(col.name, function () {
-            return col.handle.find({});
-        });
-    } else {
+    if (Meteor.isClient) {
         Meteor.subscribe(col.name);
+    } else {
+        col.publish();
     }
 }
