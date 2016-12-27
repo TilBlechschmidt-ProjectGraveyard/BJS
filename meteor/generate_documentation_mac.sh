9306964e9e51eaed1e309d7e3f5f0f323bd71171
NoahPeeters <noah.peeters@icloud.com>
2016-12-27 21:26:46 +0100

    Added Readme to hompage of documentation

--- meteor/generate_documentation_mac.sh
+++ meteor/generate_documentation_mac.sh
@@ -2,7 +2,7 @@
 echo "Collecting files..."
 FILES=$(find . -name '*.js' -print)
 echo "Generating documentation..."
-jsdoc ${FILES} -c ./jsdoc.json -d ../docs -t ./node_modules/docdash
+jsdoc ${FILES} ../README.md -c ./jsdoc.json -d ../docs -t ./node_modules/docdash
 echo "Adding files to git..."
 git add ../docs 2>> /dev/null
 echo "Done!"
