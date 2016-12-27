9442a66f3405a8d3596c62873a115d1c9b48153f
Til Blechschmidt <til@blechschmidt.de>
2016-12-27 13:08:48 +0000

    Removed compressed documentation

--- meteor/generate_documentation.sh
+++ meteor/generate_documentation.sh
@@ -3,6 +3,6 @@ echo -e "\e[0m\e[1mCollecting files...\e[90m"
 FILES=$(find . -name '*.js' -print)
 echo -e "\e[0m\e[1mGenerating documentation...\e[90m"
 ./node_modules/.bin/jsdoc ${FILES} -c ./jsdoc.json -d ../docs -t ./node_modules/docdash
-echo -e "\e[0m\e[1mCompressing documentation...\e[90m"
-tar -czvf ../documentation.tar.gz ../docs
+#echo -e "\e[0m\e[1mCompressing documentation...\e[90m"
+#tar -czvf ../documentation.tar.gz ../docs
 echo -e "\e[0m\e[1mDone!"
