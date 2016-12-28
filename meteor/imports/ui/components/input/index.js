3d1a8bcc2233cd67d8014af3d9fad644667b473c
Til Blechschmidt <til@blechschmidt.de>
2016-12-28 21:01:58 +0000

    Added #each block for athletes

--- meteor/imports/ui/components/input/index.js
+++ meteor/imports/ui/components/input/index.js
@@ -2,10 +2,41 @@ import {Template} from "meteor/templating";
 import "./index.html";
 import "./index.css";
 import {AccountManagement} from "../../../api/AccountManagement/index";
+import {Log} from "../../../api/log";
+import {COMPETITION_TYPES} from "../../../api/logic/competition_type";
+import {Athlete} from "../../../api/logic/athlete";
 
 export let input_onload = function () {
     Template.login.helpers({
         show_login: !AccountManagement.inputPermitted()
+    });
+
+    Template.input.helpers({
+        athletes: function () {
+            const log = new Log();
+            const ct = COMPETITION_TYPES[0].object;
+            const athletes = [
+                new Athlete(log, 'Hans', 'Müller', 2000, true, 'Q#z', '0', ct.maxAge, ct),
+                new Athlete(log, 'Klaus', 'Karlsen', 2000, true, 'Q#z', '0', ct.maxAge, ct)
+            ];
+
+            let athlete_list = [];
+            for (let athlete in athletes) {
+                if (!athletes.hasOwnProperty(athlete)) continue;
+                athlete = athletes[athlete];
+                athlete_list.push({id: athlete.id, name: athlete.getFullName()});
+            }
+
+            console.log("TEST", athlete_list);
+
+            return athlete_list;
+        },
+    });
+
+    Template.input.events({
+        'click li.athlete': function () {
+            console.log("HEY THERE");
+        }
     });
 
     Template.input.onRendered(function () {
