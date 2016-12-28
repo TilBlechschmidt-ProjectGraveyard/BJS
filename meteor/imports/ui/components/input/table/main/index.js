606f32fdb66aac19b190afbf76057b39979a8da2
Til Blechschmidt <til@blechschmidt.de>
2016-12-28 11:34:48 +0100

    Added functionality to logout button

--- meteor/imports/ui/components/input/table/main/index.js
+++ meteor/imports/ui/components/input/table/main/index.js
@@ -1,9 +1,9 @@
-import {Template} from 'meteor/templating';
-import './index.html';
-import './index.css';
+import {Template} from "meteor/templating";
+import "./index.html";
+import "./index.css";
 
-Template.sports_main.events({
-    'click #link_next' () {
-        FlowRouter.go('/config/athletes');
-    },
+Template.table_main.events({
+    'click .logout-button' () {
+        FlowRouter.go('/contest/login');
+    }
 });
