4ae1e2ca52100fb63b0435c4a8659407b579bba2
Til Blechschmidt <til@blechschmidt.de>
2016-12-28 14:11:58 +0000

    Added Crypto namespace

--- meteor/imports/api/database/db_example.js
+++ meteor/imports/api/database/db_example.js
@@ -1,5 +1,5 @@
 import {COMPETITION_TYPES} from "../logic/competition_type";
-import {generateAC} from "../crypto/crypto";
+import {Crypto} from "../crypto/crypto";
 import {Log} from "../log";
 import {Account} from "../logic/account";
 import {DBInterface} from "./db_access";
@@ -7,8 +7,8 @@ import {DBInterface} from "./db_access";
 module.exports = function () {
     const ct = COMPETITION_TYPES[0].object;
 
-    const groupAccount = new Account(['Q#z'], [], generateAC('1234567ljhfaljawf8', 'pepper'));
-    const serverAccount = new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], generateAC('hflhkfks;kjfjankfa', 'pepper'));
+    const groupAccount = new Account(['Q#z'], [], Crypto.generateAC('1234567ljhfaljawf8', 'pepper'));
+    const serverAccount = new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], Crypto.generateAC('hflhkfks;kjfjankfa', 'pepper'));
 
     const log = new Log();
     const ps = DBInterface.getAthletesOfAccounts(log, [groupAccount], false);
