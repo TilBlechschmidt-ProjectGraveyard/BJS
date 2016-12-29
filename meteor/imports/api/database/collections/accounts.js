86fe711be0b310abdc8dbed1eedcbfe419fb01c1
Til Blechschmidt <til@blechschmidt.de>
2016-12-29 14:01:28 +0000

    Added mock data account with pwd 1234

--- meteor/imports/api/database/collections/accounts.js
+++ meteor/imports/api/database/collections/accounts.js
@@ -8,6 +8,7 @@ Accounts.createMockData = function () {
     for (let i = 0; i < 100; i++) {
         this.handle.insert(new Account(['Q#z' + i], [], Crypto.generateAC('1234567ljhfaljawf8' + i, 'pepper')));
     }
+    this.handle.insert(new Account(['Q2b'], [], Crypto.generateAC('1234', 'chilli')));
     this.handle.insert(new Account(['Q#z'], [], Crypto.generateAC('1234567ljhfaljawf8', 'pepper')));
     this.handle.insert(new Account([], ['st_long_jump'], Crypto.generateAC('jsdhfiudhfiuahfd', 'pepper')));
     this.handle.insert(new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], Crypto.generateAC('hflhkfks;kjfjankfa', 'pepper')));
