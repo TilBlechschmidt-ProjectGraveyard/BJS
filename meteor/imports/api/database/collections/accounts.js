83d7551b68e0f0aa2be4754fef538ff98aea4912
NoahPeeters <noah.peeters@icloud.com>
2016-12-29 14:02:10 +0100

    Use simple test passwords

--- meteor/imports/api/database/collections/accounts.js
+++ meteor/imports/api/database/collections/accounts.js
@@ -6,9 +6,9 @@ export let Accounts = new Collection('Accounts', true);
 
 Accounts.createMockData = function () {
     for (let i = 0; i < 100; i++) {
-        this.handle.insert(new Account(['Q#z' + i], [], Crypto.generateAC('1234567ljhfaljawf8' + i, 'pepper')));
+        this.handle.insert(new Account(['Q#z' + i], [], Crypto.generateAC('1234' + i, 'pepper')));
     }
-    this.handle.insert(new Account(['Q#z'], [], Crypto.generateAC('1234567ljhfaljawf8', 'pepper')));
-    this.handle.insert(new Account([], ['st_long_jump'], Crypto.generateAC('jsdhfiudhfiuahfd', 'pepper')));
-    this.handle.insert(new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], Crypto.generateAC('hflhkfks;kjfjankfa', 'pepper')));
+    this.handle.insert(new Account(['Q#z'], [], Crypto.generateAC('1234', 'pepper')));
+    this.handle.insert(new Account([], ['st_long_jump'], Crypto.generateAC('4321', 'pepper')));
+    this.handle.insert(new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], Crypto.generateAC('passwort', 'pepper')));
 };
