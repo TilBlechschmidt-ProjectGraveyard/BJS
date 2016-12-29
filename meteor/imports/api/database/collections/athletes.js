ef6ca79c72d6d89e6e01b829b1d8f3a35ffaacb1
Til Blechschmidt <til@blechschmidt.de>
2016-12-29 14:51:02 +0000

    Fixed account mismatch (Again)

--- meteor/imports/api/database/collections/athletes.js
+++ meteor/imports/api/database/collections/athletes.js
@@ -10,7 +10,7 @@ export let Athletes = new Collection('Athletes', true);
 Athletes.createMockData = function () {
     const log = new Log();
     const ct = COMPETITION_TYPES[0].object;
-    const groupAccount = new Account(['Q#z'], [], Crypto.generateAC('1234', 'pepper'));
+    const groupAccount = new Account(['Q#z'], [], Crypto.generateAC('1234', 'chilli'));
     const serverAccount = new Account(['Q#z'], ['st_long_jump', 'st_ball_200', 'st_ball_200', 'st_endurance_1000', 'st_endurance_3000', 'st_sprint_100'], Crypto.generateAC('4321', 'pepper'));
     this.handle.insert(new Athlete(log, 'Hans', 'Müller', 2000, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
     this.handle.insert(new Athlete(log, 'Klaus', 'Schmidt', 1999, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
