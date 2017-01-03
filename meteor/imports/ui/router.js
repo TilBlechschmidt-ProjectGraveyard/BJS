bb548d92853c0f2717e6d94fb4dc652d37a0d21d
Til Blechschmidt <til@blechschmidt.de>
2017-01-03 22:31:13 +0000

    Added default route

--- meteor/imports/ui/router.js
+++ meteor/imports/ui/router.js
@@ -5,7 +5,7 @@ FlowRouter.route('/', {
     action: function () {
         // FlowRouter.go("/config");
         //TODO: Check if it is already configured and run the following if that is the case:
-        // FlowRouter.go("/contest");
+        FlowRouter.go("/login");
     }
 });
 
