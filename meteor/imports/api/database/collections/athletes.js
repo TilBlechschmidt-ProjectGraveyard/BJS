44ab86ef42446e0699ebf931e87e977f19695121
Til Blechschmidt <til@blechschmidt.de>
2017-01-05 03:33:04 +0100

    Added more example athletes

--- meteor/imports/api/database/collections/athletes.js
+++ meteor/imports/api/database/collections/athletes.js
@@ -21,6 +21,15 @@ export function initAthletes() {
         this.handle.insert(new Athlete(log, 'Hans', 'Müller', 2000, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
         this.handle.insert(new Athlete(log, 'Klaus', 'Schmidt', 1999, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
         this.handle.insert(new Athlete(log, 'Herbert', 'Gronewoldt', 1989, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Morten', 'Meier', 1998, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Mark', 'Forster', 1988, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Hans', 'Stüber', 1992, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Maximilian', 'Humboldt', 1996, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Hartwig', 'Grumboldt', 1994, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Hedwig', 'Potter', 1962, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Harry', 'Potter', 1960, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Dr.', 'Who', 1, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
+        this.handle.insert(new Athlete(log, 'Amy', 'Pond', 1990, true, 'Q#z', '0', ct.maxAge, ct).encryptForDatabase(groupAccount, serverAccount));
 
         // add example measurement
         const p = DBInterface.getAthletesOfAccounts(log, [groupAccount], false)[0];
@@ -35,5 +44,14 @@ export function initAthletes() {
         p2.addMeasurement(log, 'st_long_jump', [7.33], groupAccount, serverAccount);
         p2.addMeasurement(log, 'st_ball_200', [5.55], groupAccount, serverAccount);
         p2.addMeasurement(log, 'st_sprint_100', [9.32], groupAccount, serverAccount);
+
+        for (let i = 2; i < 7; i++) {
+            const p3 = DBInterface.getAthletesOfAccounts(log, [groupAccount], false)[i];
+            p3.addMeasurement(log, 'st_long_jump', [7.33], groupAccount, serverAccount);
+            p3.addMeasurement(log, 'st_ball_200', [5.55], groupAccount, serverAccount);
+            p3.addMeasurement(log, 'st_sprint_100', [9.32], groupAccount, serverAccount);
+            p3.addMeasurement(log, 'st_endurance_1000', [200], groupAccount, serverAccount);
+            p3.addMeasurement(log, 'st_endurance_3000', [540], groupAccount, serverAccount);
+        }
     };
 }
