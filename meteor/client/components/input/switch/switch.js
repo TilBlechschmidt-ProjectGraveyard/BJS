8b7b3dc3aa1b8d9d0b4dcf7542034a7566c55fcc
Til Blechschmidt <til@blechschmidt.de>
2017-01-06 15:48:44 +0000

    Added functional settings (resolves #65)

--- meteor/client/components/input/switch/switch.js
+++ meteor/client/components/input/switch/switch.js
@@ -1,5 +1,30 @@
+const defaultSettings = {
+    showMale: true,
+    showFemale: true,
+    groupBySex: false
+};
+
+Template.settingSwitch.helpers({
+    getState: function (id) {
+        let state = Session.get(id);
+        if (state === undefined)
+            state = defaultSettings[id];
+        return state;
+    }
+});
+
 Template.settingSwitch.events({
     'click .setting-checkbox': function (event) {
         console.log("Setting updated (TODO: Store this somewhere and use it): ", event.target.dataset.id, event.target.checked);
+        Session.set(event.target.dataset.id, event.target.checked);
+    }
+});
+
+// Set default options if not set
+Template.settingSwitch.onRendered(function () {
+    for (let id in defaultSettings) {
+        if (!defaultSettings.hasOwnProperty(id)) continue;
+        if (Session.get(id) === undefined)
+            Session.set(id, defaultSettings[id]);
     }
 });
