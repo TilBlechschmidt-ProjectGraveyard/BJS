fb5914061bdab5c61526e6350629693202e42dd4
Til Blechschmidt <til@blechschmidt.de>
2017-01-08 23:48:28 +0100

    Added code generation for stations

--- meteor/client/components/config/accessCodes/accessCodes.js
+++ meteor/client/components/config/accessCodes/accessCodes.js
@@ -1,23 +1,63 @@
 import {genRandomCode} from "../../../../imports/api/crypto/pwdgen";
+import {Crypto} from "../../../../imports/api/crypto/crypto";
 import {Account} from "../../../../imports/api/logic/account";
+import {currentCompID} from "../config";
+import {DBInterface} from "../../../../imports/api/database/DBInterface";
+
 let accessCodes = new ReactiveVar([
-    {
-        name: "Stationspasswörter",
-        codes: [
-            {title: "Weitsprung", code: "Galifrey1337Kartoffel"},
-            {title: "Hochsprung", code: undefined},
-        ]
-    },
-    {name: "Gruppenpasswörter"},
-    {name: "Eigene Zugangsdaten"},
+    {name: "Gruppenpasswörter", codes: []},
+    {name: "Stationspasswörter", codes: []},
+    {name: "Eigene Zugangsdaten", codes: []}
 ]);
 
+function upsertCode(code, name, type) {
+    const acodes = accessCodes.get();
+    const index = lodash.findIndex(acodes[type].codes, function (code) {
+        return code.title == name;
+    });
+    if (index == -1)
+        acodes[type].codes.push(code);
+    else
+        acodes[type].codes[index] = code;
+    accessCodes.set(acodes);
+}
+
+function setCode(code, name, groups, sportTypes, resultPermission, adminPermission) {
+    if (groups.length > 0 && sportTypes.length == 0) {
+        // Group account
+        upsertCode(code, name, 0);
+    } else if (sportTypes.length > 0 && !resultPermission && !adminPermission) {
+        // Station account
+        upsertCode(code, name, 1);
+    } else {
+        // Custom account
+        upsertCode(code, name, 2);
+    }
+}
+
 function createAccount(name, groups, sportTypes, resultPermission, adminPermission) {
+    setCode({title: name}, name, groups, sportTypes, resultPermission, adminPermission);
+
     const password = genRandomCode();
 
     const account = new Account(name, groups, sportTypes, Crypto.generateAC(password), resultPermission, adminPermission);
 
-    // TODO Dump into reactive var and store in local storage
+    setCode({
+        title: name,
+        account: account,
+        code: password
+    }, name, groups, sportTypes, resultPermission, adminPermission);
+}
+
+function processCodes(codes) {
+    console.log(codes);
+    if (codes.length > 0) {
+        const code = codes.pop();
+        setTimeout(function () {
+            createAccount(code.name, code.groups, code.sportTypes, code.resultPermission, code.adminPermission);
+            processCodes(codes);
+        }, 200);
+    }
 }
 
 Template.accessCodes.helpers({
@@ -26,29 +66,54 @@ Template.accessCodes.helpers({
     }
 });
 
-Template.accessCodes.animations({
-    ".item": {
-        container: ".container", // container of the ".item" elements
-        insert: {
-            class: "fade-open", // class applied to inserted elements
-            before: function (attrs, element, template) {
-            }, // callback before the insert animation is triggered
-            after: function (attrs, element, template) {
-            }, // callback after an element gets inserted
-            // delay: 500 // Delay before inserted items animate
-        },
-        remove: {
-            class: "fade-out", // class applied to removed elements
-            before: function (attrs, element, template) {
-                console.log("hiding");
-            }, // callback before the remove animation is triggered
-            after: function (attrs, element, template) {
-                console.log("hidden");
-            }, // callback after an element gets removed
-            // delay: 500 // Delay before removed items animate
-        },
-        animateInitial: true, // animate the elements already rendered
-        animateInitialStep: 200, // Step between animations for each initial item
-        animateInitialDelay: 500 // Delay before the initial items animate
+Template.accessCodes.events({
+    'click .generateCodes': function () {
+        const compID = currentCompID.get();
+        const competitionType = DBInterface.getCompetitionType(compID);
+        const sportTypes = lodash.map(DBInterface.getCompetitionSportTypes(compID), function (stID) {
+            return competitionType.getSportType(stID);
+        });
+
+        const codes = [];
+        for (let sportType in sportTypes) {
+            if (!sportTypes.hasOwnProperty(sportType)) continue;
+            sportType = sportTypes[sportType];
+            // console.log(sportType.name, sportType.id);
+            codes.push({
+                name: sportType.name,
+                groups: [],
+                sportTypes: [sportType.id],
+                resultPermission: false,
+                adminPermission: false
+            });
+        }
+        processCodes(codes);
     }
 });
+
+// Template.accessCodes.animations({
+//     ".item": {
+//         container: ".container", // container of the ".item" elements
+//         insert: {
+//             class: "fade-open", // class applied to inserted elements
+//             before: function (attrs, element, template) {
+//             }, // callback before the insert animation is triggered
+//             after: function (attrs, element, template) {
+//             }, // callback after an element gets inserted
+//             // delay: 500 // Delay before inserted items animate
+//         },
+//         remove: {
+//             class: "fade-out", // class applied to removed elements
+//             before: function (attrs, element, template) {
+//                 console.log("hiding");
+//             }, // callback before the remove animation is triggered
+//             after: function (attrs, element, template) {
+//                 console.log("hidden");
+//             }, // callback after an element gets removed
+//             // delay: 500 // Delay before removed items animate
+//         },
+//         animateInitial: true, // animate the elements already rendered
+//         animateInitialStep: 200, // Step between animations for each initial item
+//         animateInitialDelay: 500 // Delay before the initial items animate
+//     }
+// });
