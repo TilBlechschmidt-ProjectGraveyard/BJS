2013e97d95fa24e5c15fbe4850fe6c21215677da
Til Blechschmidt <til@blechschmidt.de>
2017-01-14 21:06:40 +0000

    Reworked sportTypeSelection

--- meteor/client/components/config/sportTypeSelection/sportTypeSelection.js
+++ meteor/client/components/config/sportTypeSelection/sportTypeSelection.js
@@ -8,34 +8,34 @@ DBInterface.waitForReady(function () {
     Tracker.autorun(function () {
         if (Meteor.f7) Meteor.f7.showIndicator();
         dbReady.depend();
-        DBInterface.waitForReady(function () {
-            const compID = currentCompID.get();
-            if (!compID) {
-                if (Meteor.f7) Meteor.f7.hideIndicator();
-                return;
-            }
-            const competition = Meteor.COLLECTIONS.Contests.handle.findOne({_id: compID});
-            const competitionType = DBInterface.getCompetitionType(compID);
-            const sportTypes = competitionType.getSports();
-            const cats = lodash.map(competitionType.getInformation().categoryNames, function (name) {
-                return {name: name, sportTypes: []};
-            });
 
-            for (let sportType in sportTypes) {
-                if (!sportTypes.hasOwnProperty(sportType)) continue;
-                sportType = sportTypes[sportType];
+        const compID = currentCompID.get();
+        if (!compID) {
+            if (Meteor.f7) Meteor.f7.hideIndicator();
+            return;
+        }
 
-                // set enabled state for sportType
-                if (lodash.includes(competition.sportTypes, sportType.id)) sportType.checked = "checked";
+        const competition = Meteor.COLLECTIONS.Contests.handle.findOne({_id: compID});
+        const competitionType = DBInterface.getCompetitionType(compID);
+        const sportTypes = competitionType.getSports();
+        const cats = lodash.map(competitionType.getInformation().categoryNames, function (name) {
+            return {name: name, sportTypes: []};
+        });
 
-                cats[sportType.category].sportTypes.push(sportType);
-            }
+        for (let sportType in sportTypes) {
+            if (!sportTypes.hasOwnProperty(sportType)) continue;
+            sportType = sportTypes[sportType];
 
-            categories.set(cats);
+            // set enabled state for sportType
+            if (lodash.includes(competition.sportTypes, sportType.id)) sportType.checked = "checked";
 
-            Tracker.afterFlush(function () {
-                if (Meteor.f7) Meteor.f7.hideIndicator();
-            });
+            cats[sportType.category].sportTypes.push(sportType);
+        }
+
+        categories.set(cats);
+
+        Tracker.afterFlush(function () {
+            if (Meteor.f7) Meteor.f7.hideIndicator();
         });
     });
 });
@@ -48,11 +48,28 @@ Template.sportTypeSelection.helpers({
 
 Template.sportTypeSelection.events({
     'click .checkbox-sportType': function (event) {
+        event.stopImmediatePropagation();
+        event.preventDefault();
         const label = event.target.closest("label");
         const checkbox = label.querySelector("input[type='checkbox']");
         const newValue = !checkbox.checked;
 
-        DBInterface.setSportTypeState(AccountManager.getAdminAccount().account, currentCompID.get(), label.dataset.id, newValue, function () {
-        });
+        DBInterface.setSportTypeState(AccountManager.getAdminAccount().account, currentCompID.get(), label.dataset.id, newValue);
+
+        const cats = categories.get();
+        for (let cat in cats) {
+            if (!cats.hasOwnProperty(cat)) continue;
+
+            const index = lodash.findIndex(cats[cat].sportTypes, function (a) {
+                return a.id == label.dataset.id;
+            });
+
+            if (index > -1) {
+                cats[cat].sportTypes[index].checked = newValue ? "checked" : undefined;
+                break;
+            }
+        }
+        categories.set(cats);
+        return false;
     }
 });
