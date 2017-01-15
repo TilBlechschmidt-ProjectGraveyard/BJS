f8c190a7b66328fc739932f96dc52b5579fd9565
Til Blechschmidt <til@blechschmidt.de>
2017-01-15 13:45:05 +0000

    Removed yet another debug printout

--- meteor/client/components/config/competitionTypeInformation/competitionTypeInformation.js
+++ meteor/client/components/config/competitionTypeInformation/competitionTypeInformation.js
@@ -4,7 +4,6 @@ import {COMPETITION_TYPES} from "../../../../imports/api/logic/competition_type"
 Template.competitionTypeInformationPopups.helpers({
     getInformation: function () {
         return _.map(COMPETITION_TYPES, function (ct) {
-            console.log(ct.object.getInformation());
             return ct.object.getInformation();
         })
     }
