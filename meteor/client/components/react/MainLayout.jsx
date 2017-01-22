5b8250c4d8a652dae065b3573b6a8dbb47bab70a
Til Blechschmidt <til@blechschmidt.de>
2017-01-22 16:44:39 +0100

    Added change button for name

--- meteor/client/components/react/MainLayout.jsx
+++ meteor/client/components/react/MainLayout.jsx
@@ -5,6 +5,20 @@ import {genUUID} from "../../../imports/api/crypto/pwdgen";
 import {Athlete as AthleteObj} from "../../../imports/api/logic/athlete";
 import {Log} from "../../../imports/api/log";
 
+function generateAthletes() {
+    const athletes = {};
+
+    for (let i = 0; i < 300; i++) {
+        let uuid = genUUID();
+        athletes[uuid] = new AthleteObj(new Log(), "Klaus" + i, "Schmidt" + i, 1999, true, "Q1y", "0", 20, [], uuid);
+    }
+
+    return athletes;
+}
+
+window.athletes = generateAthletes();
+const athletes = window.athletes;
+const athleteIDs = Object.keys(athletes);
 
 class MainLayout extends React.Component {
     render() {
@@ -21,25 +35,11 @@ class MainLayout extends React.Component {
 }
 
 class Athletes extends React.Component {
-    getAthletes() {
-        const athletes = [];
-
-        console.log("Creating athletes");
-        for (let i = 0; i < 1000; i++) {
-            athletes.push(
-                new AthleteObj(new Log(), "Klaus" + i, "Schmidt" + i, 1999, true, "Q1y", "0", 20, [], genUUID())
-            );
-        }
-        console.log("done");
-
-        return athletes;
-    }
-
     render() {
         return (
             <div className="list-block">
                 <ul>
-                    {this.getAthletes().map((athlete) => <Athlete athlete={athlete} key={athlete.id}/>)}
+                    {athleteIDs.map((uuid) => <Athlete uuid={uuid} key={uuid}/>)}
                 </ul>
             </div>
         );
@@ -47,16 +47,56 @@ class Athletes extends React.Component {
 }
 
 class Athlete extends React.Component {
+
+    constructor(...args) {
+        super(...args);
+
+        this.state = athletes[this.props.uuid];
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+        console.log("UPDATE", newProps, newState);
+        return true;
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("HANDLING CLICK");
+        this.setState({firstName: "SomethingWeird"});
+    }
+
     render() {
+        const athlete = AthleteObj.fromObject(new Log(), this.state);
         return (
             <li className="accordion-item">
                 <a href="" className="item-content item-link">
+                    <div className="item-media"><Icon/></div>
                     <div className="item-inner">
-                        <div className="item-title">{this.props.athlete.getFullName()}</div>
+                        <div className="item-title">{athlete.getFullName()}</div>
                     </div>
                 </a>
-                <div className="accordion-item-content">Item 1 content ...</div>
+                <div className="accordion-item-content">
+                    Item 1 content ...
+                    <button onMouseDown={this.handleClick}>
+                        THIS IS A NICE BUTTON
+                    </button>
+                </div>
             </li>
+        );
+    }
+}
+
+class Icon extends React.Component {
+    render() {
+        return (
+            <svg className="icon i-ok not-animated"
+                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle className="i__circle" cx="26" cy="26" r="25" fill="none"/>
+                <path className="i__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+            </svg>
         );
     }
 }
