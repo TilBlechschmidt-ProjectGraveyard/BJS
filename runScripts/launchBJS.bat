dbb8da8965087fb0521fadfdba86596d3291e5e1
Til Blechschmidt <til@blechschmidt.de>
2017-02-27 20:41:58 +0100

    Fixed windows runscript

--- runScripts/launchBJS.bat
+++ runScripts/launchBJS.bat
@@ -1,8 +1,9 @@
 cd programs/server
-npm install
+call npm install
 cd ..
 cd ..
 set MONGO_URL=mongodb://localhost:27017/BJS
 set ROOT_URL=http://localhost
 set PORT=8080
 node main.js
+set /p DUMMY=Hit ENTER to exit...
